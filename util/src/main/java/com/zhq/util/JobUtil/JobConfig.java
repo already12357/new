aa80353d70dd00f9edb254c3cc79a9687c369d0f
testname <test@qq.com>
2021-11-10 21:21:41 +0800

    git 远程分支提交修改测试

---
+++ util/src/main/java/com/zhq/util/JobUtil/JobConfig.java
@@ -0,0 +1,19 @@
+package com.zhq.util.JobUtil;
+
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 任务分发器的配置类,
+ * 用于给容器中注入对应的对象
+ */
+@Configuration
+public class JobConfig {
+    @Bean
+    public Scheduler scheduler() throws SchedulerException {
+        return StdSchedulerFactory.getDefaultScheduler();
+    }
+}
