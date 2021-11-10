8bf26db45916262f95d31cbb42b7e730d85e27eb
testname <test@qq.com>
2021-11-10 10:10:56 +0800

    git 远程分支提交修改测试

--- util/.idea/sonarlint/issuestore/index.pb
+++ util/.idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 W
 'src/test/java/com/zhq/JDBCUtilTest.java,8\a\8a77b912961ecff2da4118e0d4777e2510b9f90d
-X
-(src/main/java/com/zhq/util/FileUtil.java,5\4\5482223535327bd6f1ccf23046249a2eec190d09
 Y
 )src/test/java/com/zhq/StringUtilTest.java,5\f\5f4d38c3c56922ab734eda05be8c9ba3f3ea4a4a
 W
--- util/src/main/java/com/zhq/util/JobUtil/JobDispatcher.java
+++ util/src/main/java/com/zhq/util/JobUtil/JobDispatcher.java
@@ -3,14 +3,18 @@ package com.zhq.util.JobUtil;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
 // 用于调度发配对应的任务
+@Component
 public class JobDispatcher {
-    public static final String DEFAULT_TRIGGER_ID = "default_trigger_id";
+    public static final String TRIGGER_PREFIX = "trigger_";
+    public static final String JOB_PREFIX = "job_";
 
     // 需要注入默认的调度器
     @Autowired
@@ -24,13 +28,19 @@ public class JobDispatcher {
 
     /**
      * 向调度器中, 添加一个任务, 默认状态下, 调度器时没有运行的
-     * @param jobId 任务的编号
+     * @param Id 用于定位任务的编号
+     * @param startDate 任务的起始日期
+     * @param endDate 任务的结束日期
+     * @param minutesInterval 执行的时间间隔 ( 分钟 )
      * @param clazz 对应任务的类
      * @param args 启动任务所需的参数, 以 <参数名, 参数>
      */
-    public void addJob(String jobId, String triggerId, Date startDate, Date endDate, int interval,
+    public void addJob(String Id, Date startDate, Date endDate, int minutesInterval,
                        Class<? extends Job> clazz, Map<String, Object> args)
             throws SchedulerException {
+        // 通过 ID 拼接前缀来形成对应的编号
+        String jobId = JOB_PREFIX + Id;
+        String triggerId = TRIGGER_PREFIX + Id;
         JobDetail jobDetail = JobBuilder.newJob(clazz).withIdentity(jobId).build();
 
         // 向工作的空间中传入参数
@@ -43,19 +53,59 @@ public class JobDispatcher {
                 .withIdentity(triggerId)
                 .startAt(startDate)
                 .endAt(endDate)
-                .withSchedule(simpleScheduleBuilder.withIntervalInMinutes(interval)).build();
+                .withSchedule(simpleScheduleBuilder.withIntervalInMinutes(minutesInterval)).build();
 
         scheduler.scheduleJob(jobDetail, simpleTrigger);
-        scheduler.start();
     }
 
-    public void suspendJob(String jobId, String triggerId, Date startDate, Date endDate, int interval,
-                           Class<? extends Job> clazz, Map<String, Object> args) {
 
+
+    public void suspendJob(String Id)
+            throws SchedulerException {
+        String jobId = JOB_PREFIX + Id;
+        String triggerId = TRIGGER_PREFIX + Id;
+        JobKey jobKey = JobKey.jobKey(jobId);
+        JobDetail jobDetail = scheduler.getJobDetail(jobKey);
+
+        if (null != jobDetail) {
+            scheduler.pauseJob(jobKey);
+        }
     }
 
-    public void removeJob() {
-        TriggerKey triggerKey = TriggerKey.triggerKey()
-        scheduler.getTrigger()
+    public void resumeJob(String Id)
+            throws SchedulerException {
+        String jobId = JOB_PREFIX + Id;
+        JobKey jobKey = JobKey.jobKey(jobId);
+        JobDetail jobDetail = scheduler.getJobDetail(jobKey);
+
+        if (null != jobDetail) {
+            scheduler.resumeJob(jobKey);
+        }
+    }
+
+    public void modifyJob(String Id, Date startDate, Date endDate, int minutesInterval) {
+        String jobId = JOB_PREFIX + Id;
+        String triggerId = TRIGGER_PREFIX + Id;
+        JobKey jobKey = JobKey.jobKey(jobId);
+
+        SimpleScheduleBuilder simpleScheduleBuilder = SimpleScheduleBuilder.simpleSchedule();
+        SimpleTrigger simpleTrigger = (SimpleTrigger) TriggerBuilder.newTrigger()
+                .withIdentity(triggerId)
+                .startAt(startDate)
+                .endAt(endDate)
+                .withSchedule(simpleScheduleBuilder.withIntervalInMinutes(minutesInterval)).build();
+    }
+
+    public void removeJob(String Id) throws SchedulerException {
+        String jobId = JOB_PREFIX + Id;
+        JobKey jobKey = JobKey.jobKey(jobId);
+        JobDetail jobDetail = scheduler.getJobDetail(jobKey);
+
+        if (null != jobDetail) {
+            // 停止触发器 和 任务
+            scheduler.pauseJob(jobKey);
+            // 删除触发器对应的任务
+            scheduler.deleteJob(jobKey);
+        }
     }
 }
