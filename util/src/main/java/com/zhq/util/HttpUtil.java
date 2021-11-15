a6905978097b887a75a9c4925ed285736b81b3e2
testname <test@qq.com>
2021-11-15 15:30:42 +0800

    git 远程分支提交修改测试

--- util/src/main/java/com/zhq/util/HttpUtil.java
+++ util/src/main/java/com/zhq/util/HttpUtil.java
@@ -3,6 +3,7 @@ package com.zhq.util;
 import com.alibaba.fastjson.JSONObject;
 import jdk.internal.util.xml.impl.Input;
 import org.springframework.http.HttpRequest;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
@@ -201,12 +203,12 @@ public class HttpUtil {
         return result.toString();
     }
 
+
 //        二进制下载文件 ( 长度相关参参数 )
 //        请求
 //        Range: bytes=0-801
 //        响应
 //        Content-Range: bytes 0-800/801
-
 
     /**
      * 在处理器方法中下载对应的文件
@@ -214,7 +216,8 @@ public class HttpUtil {
      * @param response 处理器方法传入的 Servlet 原生响应
      * @param file 需要下载的文件对象
      */
-    public static void downloadFile(HttpServletRequest request, HttpServletResponse response, File file) {
+    public static void downloadFile(HttpServletRequest request, HttpServletResponse response, File file)
+            throws UnsupportedEncodingException {
         // 获取请求中的 Range, 得到对应响应中的下载大小
         String rangeContent = request.getHeader("Range");
         Long startPos = 0L;
@@ -233,19 +236,25 @@ public class HttpUtil {
 
         String contentRange = "bytes " + startPos + "-" + (endPos - 1) + "/" + endPos;
 
+
+        // 在线显示 PDF
+//        response.reset();
+//        response.setContentType(response.getContentType());
+//        response.addHeader("Content-Disposition", "inline; filename=" + file.getName());
+
+
         // 设置响应的主要内容为二进制流
         response.setContentType("application/octet-stream");
         // 添加下载文件的标识字段
         // inline / attachment inline : 将文件内容直接显示在页面
         // attachment : 弹出对话框让用户下载
-        response.addHeader("Content-Disposition", "attachment;filename=" + file.getName());
+        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(file.getName(), "UTF-8"));
         // 设置响应中下载文件的范围
         response.addHeader("Content-Range", contentRange);
         // 设置响应中下载文件的大小
         response.addHeader("Content-Length", String.valueOf(endPos));
         // 设置响应中接受数据的类型
         response.addHeader("Accept-Ranges", "bytes");
-
 
         byte[] buffer = new byte[1024];
         OutputStream out = null;
@@ -257,10 +266,12 @@ public class HttpUtil {
             in = new FileInputStream(file);
             int readLength = 0;
 
+            // 跳过已经下载的部分
+            in.skip(startPos);
+
             while ((readLength = in.read(buffer)) != -1) {
                 out.write(buffer, 0, readLength);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
