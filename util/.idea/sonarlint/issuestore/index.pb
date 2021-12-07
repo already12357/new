9ba70b6e081ee06499e46a6a1b5be79fd0d195e8
testname <test@qq.com>
2021-12-07 12:54:03 +0800

    git 远程分支提交修改测试

--- util/.idea/sonarlint/issuestore/index.pb
+++ util/.idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,13 @@
 -src/main/java/com/zhq/util/Temp/TempFile.java,6\e\6e4aa5196c94bb1de38de290f66be0e0e924e893
 Z
 *src/main/java/com/zhq/util/StringUtil.java,c\3\c3f509b5cb8bae8abd66d1fe62616b7786fb157b
+d
+4src/main/java/com/zhq/util/JDBCUtil/DBFormatter.java,3\7\37f4d658a807bfa1a70427a5693de8135e18e847
+X
+(src/main/java/com/zhq/util/JsonUtil.java,5\3\533f918ec8ada0b424997d16f90916dd2aa60408
+_
+/src/main/java/com/zhq/util/JDBCUtil/DBUtil.java,d\b\dbc447fa9f3d664930fccc0c17ab458b233e8001
+\
+,src/main/java/com/zhq/util/ResourceUtil.java,1\0\10dbd159eaf845781b9d15bb0c403fff734eec99
+W
+'src/test/java/com/zhq/JDBCUtilTest.java,8\a\8a77b912961ecff2da4118e0d4777e2510b9f90d
--- util/src/main/resources/h5-tag/js/item/itemBoot.js
+++ util/src/main/resources/h5-tag/js/item/itemBoot.js
@@ -1,16 +1,29 @@
+'use strict';
+
 $(function () {
     function init() {
-        var css_locations = [];
-        var script_locations = [];
-        var css_item = document.createElement("link");
-        var script_item = document.createElement("script");
+        // 配置导入地址
+        let cssHref_arr = [ "./h5-tag/css/item/item.css" ];
+        let scriptSrc_arr = [ "./h5-tag/js/item/item.js" ];
 
-        script_item.type = "text/javascript";
-        script_item.src = "./item.js";
+        let css_item = document.createElement("link");
+        let script_item = document.createElement("script");
 
-        $("body").append(script_item);
+        // 遍历 js 和 css 路径, 将其拼接到对应的标签后
+        for (let i = 0; i < scriptSrc_arr.length; i++) {
+            let appendedScript = document.createElement("script");
+            appendedScript.type = "text/javascript";
+            appendedScript.src = scriptSrc_arr[i];
+            $("html").append(appendedScript);
+        }
 
+        for (let j = 0; j < cssHref_arr.length; j++) {
+            let appendedCss = document.createElement("link");
+            appendedCss.href = cssHref_arr[j];
+            appendedCss.rel = "stylesheet";
+            $("head").append(appendedCss);
+        }
     }
 
     init();
-})
+});
