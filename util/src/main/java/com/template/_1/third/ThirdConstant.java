7010a852296dccd51025eb1f7a06cdfe1382a72d
testname <test@qq.com>
2022-01-05 17:04:01 +0800

    文档自动接口优化

--- util/src/main/java/com/template/_1/third/ThirdConstant.java
+++ util/src/main/java/com/template/_1/third/ThirdConstant.java
@@ -12,34 +12,65 @@ public class ThirdConstant {
     /**
      * 接口 (1) [ 变量名 NAME1 ]
      */
-    // 接口名称 ( URL_[***] )
-    public static final String URL_NAME1 = "";
-    // 接口测试数据 ( TEST_DATA_[***] ) , 以函数形式返回
-    public static String TEST_DATA_NAME1() {
+    // 接口的中文名称
+    public static final String CHI_NAME1 = "企业信息查询接口";
+    // 接口的描述信息
+    public static final String DESCRIPTION_NAME1 = "该内容是企业查询接口的接口内容，用于生成对应的企业接口内容";
+    // 接口 URL ( URL_[***] )
+    public static final String URL_NAME1 = "http://localhost:8090/epoint-web-znsb/frame/page/rest/controller";
+    // 接口请求类型 ( METHOD_[***] )
+    public static final String METHOD_NAME1 = "post";
+    // 数据返回类型 ( DATA_TYPE_[***] )
+    public static final String DATA_TYPE_NAME1 = "";
+    // 接口测试请求数据 ( TEST_DATA_[***] ) , 以函数形式返回, 用于生成接口文档
+    public static final String TEST_REQ_DATA_NAME1() {
         JSONObject testData = new JSONObject();
 
         /**
-         * 拼接测试数据
+         * 拼接测试数据, 直接写死
          */
+        testData.put("hello", "yest");
 
-        return testData.toJSONString();
+        return JSONObject.toJSONString(testData, true);
     }
-    // 接口请求类型 ( METHOD_[***] )
-    public static final String METHOD_NAME1 = "";
-    // 数据返回类型 ( DATA_TYPE_[***] )
-    public static final String DATA_TYPE_NAME1 = "";
+    // 接口测试返回数据 ( TEST_DATA_[***] ) , 以函数形式返回, 用于生成接口文档
+    public static final String TEST_RES_DATA_NAME1() {
+        JSONObject testData = new JSONObject();
+
+        /**
+         * 拼接返回数据, 直接写死
+         */
+        testData.put("fdsa", "faf");
+
+        return JSONObject.toJSONString(testData, true);
+    }
+
+
 
     /**
      * 接口 (2) [ 变量名 NAME2 ]
      */
+    public static final String CHI_NAME2 = "";
+    public static final String DESCRIPTION_NAME2 = "";
     public static final String URL_NAME2 = "";
-    public static String TEST_DATA_NAME2() {
-        JSONObject testData = new JSONObject();
-        /**
-         * 拼接测试数据
-         */
-        return testData.toJSONString();
-    }
     public static final String METHOD_NAME2 = "";
     public static final String DATA_TYPE_NAME2 = "";
+    public static final String TEST_REQ_DATA_NAME2() {
+        JSONObject testData = new JSONObject();
+
+        /**
+         * 拼接测试数据, 直接写死
+         */
+
+        return testData.toJSONString();
+    }
+    public static final String TEST_RES_DATA_NAME2() {
+        JSONObject testData = new JSONObject();
+
+        /**
+         * 拼接返回数据, 直接写死
+         */
+
+        return testData.toJSONString();
+    }
 }
--- util/src/main/java/com/template/_1/util/TemplateUtil.java
+++ util/src/main/java/com/template/_1/util/TemplateUtil.java
@@ -1,6 +1,13 @@
 package com.template._1.util;
 
+import com.template._1.third.ThirdConstant;
+import com.zhq.util.AsposeUtil;
+
+import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 代码模板的帮助类,
@@ -10,26 +17,42 @@ public class TemplateUtil {
     /**
      * 接口模板的位置
      */
-    public static final String TEMPLATE1_LOCATION = "classpath:file/template/_1/interface_doc1.docx";
-
+    public static final String TEMPLATE_LOCATION = "src/main/resources/file/template/_1/interface_doc1.docx";
 
     /**
-     * 根据对应接口，生成对应的接口文档, 替换对应的域
-     * 使用, appose 或 poi
-     * @param restController 第三方控制器接口的名称
-     * @param methodName 调用的接口方法名称
+     * 渲染后的接口模板的生成位置 ( 默认桌面上 )
      */
-    public static void interfaceDocument(Object restController, String methodName) {
-        Class restClass = restController.getClass();
-        Method interMethod = restClass.getMethod(methodName, );
+    public static final String RENDER_LOCATION = "C:/Users/Administrator/Desktop/interface_test.docx";
 
+    /**
+     * 根据对应接口，生成对应的接口文档, 替换对应的域,
+     * 可以使用 ThirdConstant 中的常量
+     */
+    public static void interfaceDocument() {
+        Map<String, String> docFieldsMap = new HashMap<String, String>();
+
+        /**
+         * 根据 ThirdConstant 中定义的常量直接赋值，
+         * 使用 Aspose 直接替换模板中的域
+         */
+        docFieldsMap.put("interface_name", ThirdConstant.CHI_NAME1);
+        docFieldsMap.put("interface_url", ThirdConstant.URL_NAME1);
+        docFieldsMap.put("interface_description", ThirdConstant.DESCRIPTION_NAME1);
+        docFieldsMap.put("interface_reqdata", ThirdConstant.TEST_REQ_DATA_NAME1());
+        docFieldsMap.put("interface_retdata", ThirdConstant.TEST_RES_DATA_NAME1());
+
+        File templateDoc = new File(TEMPLATE_LOCATION);
+        File renderDoc = new File(RENDER_LOCATION);
+        AsposeUtil.replaceFieldsToDoc(templateDoc, renderDoc, docFieldsMap);
     }
+
+
 
     /**
      * 在类内部直接调用对应的接口内容，用于事项对应的效果
      * @param args
      */
     public static void main(String[] args) {
-
+        interfaceDocument();
     }
 }
